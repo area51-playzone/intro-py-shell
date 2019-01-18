043355f2af699e2b0bd26f164936b10479b3a175
Syed Ishtiaque Ahmad <ishtiaque05@gmail.com>
2019-01-18 16:18:40 +0600

    Add test case

---
+++ tests/convert-tests.py
@@ -0,0 +1,10 @@
+from unittest import TestCase
+import subprocess
+
+class convertTest(TestCase):
+
+    def convert_one_hundred(self):
+        command = "convert 100"
+        output = subprocess.Popen(command, stdout=subprocess.PIPE, shell=True)
+        (out, err) = output.communicate()
+        self.assertEqual(out,'one hundred\n')
