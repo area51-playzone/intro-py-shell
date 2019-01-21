32ae87c940a1a0ac18b8daa543bf9cab62848f69
Syed Ishtiaque Ahmad <ishtiaque05@gmail.com>
2019-01-21 09:50:29 +0600

    Fix installation error

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@ from setuptools import setup , find_packages
 setup (
 	name = 'CliApp',
 	description = 'An application for converting numbers to words.',
-	version = '0.10',
+	version = '0.11',
 	packages = find_packages(), # list of all packages
     	install_requires = ['inflect'],
     	python_requires='>=2.7', # any python greater than 2.7
 	test_suite="tests", # where to find tests
 	entry_points = {
 		'console_scripts': [
-			'convert_num = convert_num.__main__:main', # got to module convert.__main__ and run the method called main
+			'convertnum = convertnum.__main__:main', # got to module convert.__main__ and run the method called main
 			'app2 = app2.__main__:main'
 			]
 		}
